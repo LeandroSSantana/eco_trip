1377bb2aebab7a3c6e07ed6bef19e90af9890ce0
Leandro Santana <leandrosantana403@gmail.com>
2023-09-16 05:29:52 -0300

    refatoração na classe perfil_page

--- lib/screens/profile/perfil_page.dart
+++ lib/screens/profile/perfil_page.dart
@@ -1,122 +1,152 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:teste_telas/data/perfil_data.dart';
 import 'package:teste_telas/model/profile/perfil_usuario_model.dart';
-import 'package:teste_telas/screens/home_page/home_page.dart'; // Importe este pacote para controlar o teclado
 
 class PerfilPage extends StatelessWidget {
   final PerfilUsuario perfil;
 
   PerfilPage({required this.perfil});
 
+  Container buildModifierContainer(String text) {
+    return Container(
+      width: 215,
+      height: 45,
+      margin: EdgeInsets.only(bottom: 10),
+      decoration: BoxDecoration(
+        color: Color(0xB20047AB),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Center(
+        child: Text(
+          text,
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Row buildRowWithIcon(IconData iconData, String text) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          iconData,
+          color: Color(0xFF0047AB),
+          size: 30,
+        ),
+        SizedBox(width: 15),
+        buildModifierContainer(text),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Color(0xFFFFd191),
-        selectedLabelStyle: TextStyle(color: Colors.blue), // Cor do rótulo ativo
-        unselectedLabelStyle: TextStyle(color: Colors.white),
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(
-              CupertinoIcons.money_dollar_circle,
-              color: Color(0xFFFF8c00),
-            ),
-            label: 'Orçamento',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              CupertinoIcons.map_pin_ellipse,
-              color: Color(0xFFFF8c00),
-            ),
-            label: 'Destino',
-          ),
-        ],
-      ),
-      body: Stack(
-        children: [
-          Container(
-            color: Colors.orangeAccent,
-            // color: Color(0xFFe0e0e0),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(bottom: 100),
-            child: Container(
-              decoration: BoxDecoration(
-                  color: Colors.white38,
-                  borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(60),
-                      bottomRight: Radius.circular(60))),
-            ),
-          ),
-          Container(
-            height: (250),
-            decoration: BoxDecoration(
-                color: Color(0xFFFF8c00),
-                borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(60),
-                    bottomRight: Radius.circular(60))),
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 32),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Icon(
-                          Icons.menu_outlined,
-                          size: 30,
+    // ícones que deseja usar
+    List<IconData> icons = [
+      Icons.calendar_month_rounded,
+      Icons.monetization_on_outlined,
+      Icons.pin_drop_rounded,
+      Icons.tips_and_updates_outlined,
+    ];
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Column(
+          children: [
+            Container(
+              color: Color(0xFF0047AB),
+              width: double.infinity,
+              height: 220,
+              margin: EdgeInsets.only(bottom: 10),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(top: 10),
+                      width: 125,
+                      height: 125,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(
                           color: Colors.white,
+                          width: 3.0,
                         ),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Icon(
-                          Icons.more_horiz,
-                          size: 30,
-                          color: Colors.white,
+                      child: ClipOval(
+                        child: Image.network(
+                          perfilUsuario.fotoPerfil,
+                          width: 110,
+                          height: 110,
+                          fit: BoxFit.cover,
                         ),
-                      ),
-                    ],
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.topLeft,
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 16.0),
-                    child: Text(
-                      'Olá ${perfilUsuario.nomeUsuario} !',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontFamily: 'Rubik',
-                        fontWeight: FontWeight.w800,
-                        fontSize: 28,
                       ),
                     ),
-                  ),
-                )
-              ],
-            ),
-          ),
-          Positioned(
-              top: 180,
-              left: 16,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(16.0),
-                child: Image.network(
-                  perfilUsuario.fotoPerfil,
-                  // scale: 3.8,
-                  width: 120,
-                  height: 150,
-                  fit: BoxFit.cover,
+                    SizedBox(height: 8),
+                    Text(
+                      perfilUsuario.nomeUsuario,
+                      style: TextStyle(
+                        fontFamily: 'Rubik',
+                        fontSize: 24,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
                 ),
-              ))
-        ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(bottom: 10, left: 10, right: 10),
+              height: 350,
+              decoration: BoxDecoration(
+                color: Color(0xFFD5D9E0),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  buildRowWithIcon(icons[0], 'Datas'),
+                  buildRowWithIcon(icons[1], 'Orçamento'),
+                  buildRowWithIcon(icons[2], 'Local'),
+                  buildRowWithIcon(icons[3], 'Estilo de viagem'),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              width: 300,
+              height: 60,
+              margin: EdgeInsets.symmetric(horizontal: 50),
+              child: ElevatedButton(
+                onPressed: () {
+                  // Ação do botão
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFF0047AB).withAlpha(190),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                ),
+                child: Text(
+                  'viajar',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontFamily: 'Rubik',
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
